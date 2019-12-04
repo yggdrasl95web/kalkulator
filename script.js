7b0108295ff3b56ed9b99a475ab5aa850b202cb7
dante12588 <przemek.cwiertka@gmail.com>
2019-12-04 20:29:52 +0100

    js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+let flag_dot = 0;
+
 function disp(number){
     let pom = document.querySelector('.display').value;
-    document.querySelector('.display').value = pom + number;
+
+
+    if(number == '.' && flag_dot == 0){
+        document.querySelector('.display').value = pom + number;
+        flag_dot++;
+    }
+    else{
+        document.querySelector('.display').value = pom + number;
+    }
+
 }
